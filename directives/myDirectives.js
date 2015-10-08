e111f2dca514c1a888b474ca104d876fc1acd2a4
Don Schemmel <dons500@gmail.com>
2015-10-07 20:42:22 -0700

    Fix night mode directive

--- directives/myDirectives.js
+++ directives/myDirectives.js
@@ -3,3 +3,18 @@ nickFlix.directive("hometext", function() {
     template: "Back to the beginning"
   }
 });
+
+nickFlix.directive("toggleBodyClass", function($document) {
+  return function (scope, element, attrs) {
+    element.bind("click", function() {
+      $document.find("body").toggleClass(attrs.toggleBodyClass);
+    });
+  }
+});
+nickFlix.directive("changeClass", function() {
+  return function (scope, element) {
+    element.bind("click", function() {
+      element.toggleClass("bg-danger");
+    });
+  }
+});
